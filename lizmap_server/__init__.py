e94eaba9b355e6705c54362017c193475aaf1a25
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-05-04 16:11:58 +0200

    Remove the server folder

--- lizmap_server/__init__.py
+++ lizmap_server/__init__.py
@@ -1,63 +1,31 @@
-"""
-/***************************************************************************
- Lizmap
-                 A QGIS plugin
- Publication plugin for Lizmap web application, by 3liz.com
-                -------------------
-    begin        : 2011-11-01
-    copyright      : (C) 2011 by 3liz
-    email        : info@3liz.com
- ***************************************************************************/
+__copyright__ = 'Copyright 2022, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
 
-/****** BEGIN LICENSE BLOCK *****
- Version: MPL 1.1/GPL 2.0/LGPL 2.1
 
- The contents of this file are subject to the Mozilla Public License Version
- 1.1 (the "License"); you may not use this file except in compliance with
- the License. You may obtain a copy of the License at
- http://www.mozilla.org/MPL/
+# noinspection PyPep8Naming
+def classFactory(iface):
+    from qgis.PyQt.QtWidgets import QMessageBox
 
- Software distributed under the License is distributed on an "AS IS" basis,
- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- for the specific language governing rights and limitations under the
- License.
+    class Nothing:
 
- The Original Code is 3liz code,
+        def __init__(self, iface):
+            """ In QGIS Desktop.
+            :param iface: The QGIS Desktop interface
+            """
+            self.iface = iface
 
- The Initial Developer of the Original Code are René-Luc D'Hont rldhont@3liz.com
- and Michael Douchin mdouchin@3liz.com
- Portions created by the Initial Developer are Copyright (C) 2011
- the Initial Developer. All Rights Reserved.
+        def initGui(self):
+            QMessageBox.warning(
+                self.iface.mainWindow(),
+                'Lizmap server plugin',
+                'Lizmap server is plugin for QGIS Server. For QGIS Desktop, use the other plugin called "Lizmap".',
+            )
 
- Alternatively, the contents of this file may be used under the terms of
- either of the GNU General Public License Version 2 or later (the "GPL"),
- or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- in which case the provisions of the GPL or the LGPL are applicable instead
- of those above. If you wish to allow use of your version of this file only
- under the terms of either the GPL or the LGPL, and not to allow others to
- use your version of this file under the terms of the MPL, indicate your
- decision by deleting the provisions above and replace them with the notice
- and other provisions required by the GPL or the LGPL. If you do not delete
- the provisions above, a recipient may use your version of this file under
- the terms of any one of the MPL, the GPL or the LGPL.
+        def unload(self):
+            pass
 
- ***** END LICENSE BLOCK *****
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
-"""
-
-try:
-    from lizmap.definitions.definitions import LwcVersions
-
-    DEFAULT_LWC_VERSION = LwcVersions.Lizmap_3_5
-
-    def classFactory(iface):
-        from lizmap.plugin import Lizmap
-        return Lizmap(iface)
-
-except ImportError:
-    # We may not have this package when running on a server.
-    pass
+    return Nothing(iface)
 
 
 def serverClassFactory(serverIface):  # pylint: disable=invalid-name
@@ -66,5 +34,5 @@ def serverClassFactory(serverIface):  # pylint: disable=invalid-name
     :param serverIface: A QGIS Server interface instance.
     :type serverIface: QgsServerInterface
     """
-    from lizmap.server.lizmap_server import LizmapServer
+    from lizmap_server.plugin import LizmapServer
     return LizmapServer(serverIface)
--- lizmap_server/tools.py
+++ lizmap_server/tools.py
@@ -1,187 +1,63 @@
-"""
-/***************************************************************************
- Lizmap
-                                 A QGIS plugin
- Publication plugin for Lizmap web application, by 3liz.com
-                                -------------------
-        begin                : 2011-11-01
-        copyright            : (C) 2011 by 3liz
-        email                : info@3liz.com
- ***************************************************************************/
+__copyright__ = 'Copyright 2021, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
 
-/****** BEGIN LICENSE BLOCK *****
- Version: MPL 1.1/GPL 2.0/LGPL 2.1
-
- The contents of this file are subject to the Mozilla Public License Version
- 1.1 (the "License"); you may not use this file except in compliance with
- the License. You may obtain a copy of the License at
- http://www.mozilla.org/MPL/
-
- Software distributed under the License is distributed on an "AS IS" basis,
- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- for the specific language governing rights and limitations under the
- License.
-
- The Original Code is 3liz code,
-
- The Initial Developer of the Original Code are René-Luc D'Hont rldhont@3liz.com
- and Michael Douchin mdouchin@3liz.com
- Portions created by the Initial Developer are Copyright (C) 2011
- the Initial Developer. All Rights Reserved.
-
- Alternatively, the contents of this file may be used under the terms of
- either of the GNU General Public License Version 2 or later (the "GPL"),
- or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- in which case the provisions of the GPL or the LGPL are applicable instead
- of those above. If you wish to allow use of your version of this file only
- under the terms of either the GPL or the LGPL, and not to allow others to
- use your version of this file under the terms of the MPL, indicate your
- decision by deleting the provisions above and replace them with the notice
- and other provisions required by the GPL or the LGPL. If you do not delete
- the provisions above, a recipient may use your version of this file under
- the terms of any one of the MPL, the GPL or the LGPL.
-
- ***** END LICENSE BLOCK ***** */
-"""
-
+import configparser
 import os
-import subprocess
-import unicodedata
-import urllib.parse
 
-from os.path import abspath, join
+from pathlib import Path
+from typing import Union
 
-from qgis.core import QgsApplication, QgsProviderRegistry, QgsVectorLayer
-from qgis.PyQt.QtCore import QDir
+from qgis.core import Qgis, QgsMessageLog
 
-from lizmap.definitions.definitions import LayerProperties
-
-
-def unaccent(a_string: str) -> str:
-    """ Return the unaccentuated string. """
-    return ''.join(
-        (c for c in unicodedata.normalize('NFD', a_string) if unicodedata.category(c) != 'Mn'))
+"""
+Tools for Lizmap.
+"""
 
 
-def get_layer_wms_parameters(layer):
-    """
-    Get WMS parameters for a raster WMS layers
-    """
-    uri = layer.dataProvider().dataSourceUri()
-    # avoid WMTS layers (not supported yet in Lizmap Web Client)
-    if 'wmts' in uri or 'WMTS' in uri:
-        return None
-
-    # Split WMS parameters
-    wms_params = dict((p.split('=') + [''])[:2] for p in uri.split('&'))
-
-    # urldecode WMS url
-    wms_params['url'] = urllib.parse.unquote(wms_params['url']).replace('&&', '&').replace('==', '=')
-
-    return wms_params
-
-
-def is_database_layer(layer) -> bool:
-    """ Check if the layer is a database layer.
-
-    It returns True for postgres, spatialite and gpkg files.
-    """
-    if layer.providerType() in ('postgres', 'spatialite'):
-        return True
-
-    uri = QgsProviderRegistry.instance().decodeUri('ogr', layer.source())
-    extension = os.path.splitext(uri['path'])[1]
-    if extension.lower() == '.gpkg':
-        return True
-
-    return False
-
-
-def layer_property(layer: QgsVectorLayer, item_property: LayerProperties) -> str:
-    if item_property == LayerProperties.DataUrl:
-        return layer.dataUrl()
+def to_bool(val: Union[str, int, float, bool], default_value: bool = True) -> bool:
+    """ Convert lizmap config value to boolean """
+    if isinstance(val, str):
+        # For string, compare lower value to True string
+        return val.lower() in ('yes', 'true', 't', '1')
+    elif not val:
+        # For value like False, 0, 0.0, None, empty list or dict returns False
+        return False
     else:
-        raise NotImplementedError
+        return default_value
 
 
-def format_qgis_version(qgis_version) -> tuple:
-    """ Split a QGIS int version number into major, minor, bugfix.
-
-     If the minor version is a dev version, the next stable minor version is set.
-     """
-    qgis_version = str(qgis_version)
-    major = int(qgis_version[0])
-    minor = int(qgis_version[1:3])
-    if minor % 2:
-        minor += 1
-    bug_fix = int(qgis_version[3:])
-    return major, minor, bug_fix
-
-
-def lizmap_user_folder() -> str:
-    """ Get the Lizmap user folder.
-
-    If the folder does not exist, it will create it.
-
-    On Linux: .local/share/QGIS/QGIS3/profiles/default/Lizmap
-    """
-    path = abspath(join(QgsApplication.qgisSettingsDirPath(), 'Lizmap'))
-
-    if not QDir(path).exists():
-        QDir().mkdir(path)
-
-    return path
+def version() -> str:
+    """ Returns the Lizmap current version. """
+    # Do not use qgis_plugin_tools because of the packaging
+    file_path = Path(__file__).parent.parent.joinpath('metadata.txt')
+    config = configparser.ConfigParser()
+    try:
+        config.read(file_path, encoding='utf8')
+    except UnicodeDecodeError:
+        # Issue LWC https://github.com/3liz/lizmap-web-client/issues/1908
+        # Maybe a locale issue ?
+        # Do not use logger here, circular import
+        # noinspection PyTypeChecker
+        QgsMessageLog.logMessage(
+            "Error, an UnicodeDecodeError occurred while reading the metadata.txt. Is the locale "
+            "correctly set on the server ?",
+            "Lizmap", Qgis.Critical)
+        return 'NULL'
+    else:
+        return config["general"]["version"]
 
 
-def current_git_hash() -> str:
-    """ Retrieve the current git hash number of the git repo (first 6 digit). """
-    repo_dir = os.path.dirname(os.path.abspath(__file__))
-    git_show = subprocess.Popen(
-        'git rev-parse --short=6 HEAD',
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-        cwd=repo_dir,
-        universal_newlines=True,
-        encoding='utf8'
-    )
-    hash_number = git_show.communicate()[0].partition('\n')[0]
-    if hash_number == '':
-        hash_number = 'unknown'
-    return hash_number
+def check_environment_variable() -> bool:
+    """ Check the server configuration. """
+    if not to_bool(os.environ.get('QGIS_SERVER_LIZMAP_REVEAL_SETTINGS', ''), default_value=False):
+        QgsMessageLog.logMessage(
+            'Please read the documentation how to enable the Lizmap API on QGIS server side '
+            'https://docs.lizmap.com/current/en/install/pre_requirements.html#lizmap-server-plugin '
+            'An environment variable must be enabled to have Lizmap Web Client ≥ 3.5.',
+            "Lizmap",
+            Qgis.Critical
+        )
+        return False
 
-
-def has_git() -> bool:
-    """ Using Git command, trying to know if we are in a git directory. """
-    repo_dir = os.path.dirname(os.path.abspath(__file__))
-    git_show = subprocess.Popen(
-        'git rev-parse --git-dir',
-        stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL,
-        shell=True,
-        cwd=repo_dir,
-        universal_newlines=True,
-        encoding='utf8'
-    )
-    output = git_show.communicate()[0].partition('\n')[0]
-    return output != ''
-
-
-def next_git_tag():
-    """ Using Git command, trying to guess the next tag. """
-    repo_dir = os.path.dirname(os.path.abspath(__file__))
-    git_show = subprocess.Popen(
-        'git describe --tags $(git rev-list --tags --max-count=1)',
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-        cwd=repo_dir,
-        universal_newlines=True,
-        encoding='utf8'
-    )
-    tag = git_show.communicate()[0].partition('\n')[0]
-    if not tag:
-        return 'next'
-    versions = tag.split('.')
-    text = '{}.{}.{}-pre'.format(versions[0], versions[1], int(versions[2]) + 1)
-    return text
+    return True
