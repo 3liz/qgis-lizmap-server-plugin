33ecd18364651628e31728a89efb0e86fd2b83e3
rldhont <rldhont@gmail.com>
2022-09-15 00:31:42 +0200

    Test: filter by polygon WMS

--- test/test_lizmap_accesscontrol.py
+++ test/test_lizmap_accesscontrol.py
@@ -2,10 +2,12 @@ __copyright__ = 'Copyright 2021, 3Liz'
 __license__ = 'GPL version 3'
 __email__ = 'info@3liz.org'
 
+import io
 import logging
 
 from pathlib import Path
 
+from PIL import Image
 from qgis.core import QgsVectorLayer
 
 LOGGER = logging.getLogger('server')
@@ -222,6 +224,143 @@ def test_filter_by_polygon_wfs_getfeature(client):
     layers = rv.xpath('//gml:featureMember')
     assert layers is not None
     assert len(layers) == 1
+
+
+def test_filter_by_polygon_wms(client):
+    project = "test_filter_layer_data_by_polygon_for_groups.qgs"
+
+    # Transparent GetMap
+    qs = '?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=shop_bakery&STYLES=d%C3%A9faut'
+    qs += '&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&DPI=96&TRANSPARENT=TRUE'
+    qs += '&CRS=EPSG%3A3857&BBOX=429949.78991818504,5413886.938768325,432661.7745088209,5416400.485462084&WIDTH=410&HEIGHT=380'
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('image/png') == 0
+
+    img = Image.open(io.BytesIO(rv.content))
+    # save image for debugging
+    # img.save(client.datapath.join('transparent.png').strpath)
+    assert img.format == 'PNG'
+    assert img.width == 410
+    assert img.height == 380
+    # getcolors returns None if the image contains more than maxcolors
+    # so with maxcolors = 1, getcolors returns None if the image is not
+    # a single color image
+    colors = img.getcolors(1)
+    assert colors is not None
+    assert len(colors) == 1
+    # single color, every pixel are the same
+    assert colors[0][0] == 410 * 380
+    assert colors[0][1][0] == 0
+    assert colors[0][1][1] == 0
+    assert colors[0][1][2] == 0
+    assert colors[0][1][3] == 0
+
+    # WMS GetMap with 2 points (Montferrier-sur-Lez and Castries)
+    qs = '?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=shop_bakery&STYLES=d%C3%A9faut'
+    qs += '&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&DPI=96&TRANSPARENT=TRUE'
+    qs += '&CRS=EPSG%3A3857&BBOX=429287.80910808814,5411955.107681999,431999.793698724,5414468.654375758&WIDTH=410&HEIGHT=380'
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('image/png') == 0
+
+    img = Image.open(io.BytesIO(rv.content))
+    # save image for debugging
+    # img.save(client.datapath.join('Montferrier-Castries.png').strpath)
+    assert img.format == 'PNG'
+    assert img.width == 410
+    assert img.height == 380
+    # remove transparency to reduce the number of colors
+    # img = img.convert('RGB')
+    colors = img.getcolors()
+    assert colors is not None
+    colors.sort(key=lambda color: color[0], reverse=True)
+    # more colors than for the transparency
+    assert len(colors) == 21
+    # less than 99,899% for the most important color
+    assert colors[0][0] < 410 * 380 * 0.99899
+    # more than 99,799% for the most important color
+    assert colors[0][0] > 410 * 380 * 0.99799
+    assert colors[0][1][0] == 0
+    assert colors[0][1][1] == 0
+    assert colors[0][1][2] == 0
+    assert colors[0][1][3] == 0
+
+    # Filter
+    qs += "&LIZMAP_USER_GROUPS=montferrier-sur-lez"
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('image/png') == 0
+
+    img = Image.open(io.BytesIO(rv.content))
+    # save image for debugging
+    # img.save(client.datapath.join('Montferrier.png').strpath)
+    assert img.format == 'PNG'
+    assert img.width == 410
+    assert img.height == 380
+    # remove transparency to reduce the number of colors
+    # img = img.convert('RGB')
+    colors = img.getcolors()
+    assert colors is not None
+    colors.sort(key=lambda color: color[0], reverse=True)
+    # more colors than for the transparency
+    assert len(colors) == 21
+    # more than 99,899% for the most important color
+    assert colors[0][0] > 410 * 380 * 0.99899
+    assert colors[0][1][0] == 0
+    assert colors[0][1][1] == 0
+    assert colors[0][1][2] == 0
+    assert colors[0][1][3] == 0
+
+    # Montferrier sur Lez
+    qs = '?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&LAYERS=shop_bakery&QUERY_LAYERS=shop_bakery&STYLES=d%C3%A9faut'
+    qs += '&FEATURE_COUNT=10&BBOX=429287.80910808814,5411955.107681999,431999.793698724,5414468.654375758&WIDTH=410&HEIGHT=380'
+    qs += '&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&INFO_FORMAT=text%2Fxml&CRS=EPSG%3A3857&I=95&J=12'
+    qs += '&FI_POINT_TOLERANCE=25&FI_LINE_TOLERANCE=10&FI_POLYGON_TOLERANCE=5'
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('text/xml') == 0
+    assert rv.xml.tag == 'GetFeatureInfoResponse'
+
+    features = rv.xpath('//Feature')
+    assert features is not None
+    assert len(features) == 1
+
+    # Filter
+    qs += "&LIZMAP_USER_GROUPS=montferrier-sur-lez"
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('text/xml') == 0
+    assert rv.xml.tag == 'GetFeatureInfoResponse'
+
+    features = rv.xpath('//Feature')
+    assert features is not None
+    assert len(features) == 1
+
+    # Castries
+    qs = '?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&LAYERS=shop_bakery&QUERY_LAYERS=shop_bakery&STYLES=d%C3%A9faut'
+    qs += '&FEATURE_COUNT=10&BBOX=429287.80910808814,5411955.107681999,431999.793698724,5414468.654375758&WIDTH=410&HEIGHT=380'
+    qs += '&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&INFO_FORMAT=text%2Fxml&CRS=EPSG%3A3857&I=372&J=338'
+    qs += '&FI_POINT_TOLERANCE=25&FI_LINE_TOLERANCE=10&FI_POLYGON_TOLERANCE=5'
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('text/xml') == 0
+    assert rv.xml.tag == 'GetFeatureInfoResponse'
+
+    features = rv.xpath('//Feature')
+    assert features is not None
+    assert len(features) == 1
+
+    # Filter
+    qs += "&LIZMAP_USER_GROUPS=montferrier-sur-lez"
+    rv = client.get(qs, project)
+    assert rv.status_code == 200
+    assert rv.headers.get('Content-Type', '').find('text/xml') == 0
+    assert rv.xml.tag == 'GetFeatureInfoResponse'
+
+    features = rv.xpath('//Feature')
+    assert features is not None
+    assert len(features) == 0
 
 
 def test_group_visibility_headers(client):
