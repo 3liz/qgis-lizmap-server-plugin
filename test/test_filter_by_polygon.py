d515bf70f69261cfc8a7973cd053476d1ae6d7e3
rldhont <rldhont@gmail.com>
2022-09-30 15:45:26 +0200

    Reactivate layerFilterSubsetString and deactivate layerFilterExpression (#21)
    * Reactivate layerFilterSubsetString and deactivate layerFilterExpression
    
    Because of `QgsAccessControl` stored filter expressions with resolveFilterFeatures used in WMS
    and does not clear filter expressions cached at the end of request.
    
    Because of `QgsFeatureRequest` could not be `FilterExpression` and `FilterFids`, `QgsAccessControl`
    filter expressions should not be apply to QgsFeatureRequest when it's type is `FilterFids`.
    
    Lizmap cannot use filter expressions and has to use filter subsetstring.
    
    * Fix tests
    
    * Python - Run isort
    
    Co-authored-by: Etienne Trimaille <gustrimaille@yahoo.fr>

--- test/test_filter_by_polygon.py
+++ test/test_filter_by_polygon.py
@@ -168,7 +168,7 @@ class TestFilterByPolygon(unittest.TestCase):
 
         groups = ('admins',)
         subset, ewkt = config.subset_sql(groups)
-        self.assertEqual('1 = 1', subset)
+        self.assertEqual('', subset)
         self.assertEqual('', ewkt)
 
         config = FilterByPolygon(json, points, editing=True)
