7a3ce18bf4c1915d40bb8b18da27b05c1bc8e02a
Michael Douchin <mdouchin@3liz.com>
2026-07-20 10:36:52 +0200

    Tooltip (popup autogenerated HTML from drag&drop designer) - Improve legend UI (Bootstrap 5)

--- lizmap_server/tooltip.py
+++ lizmap_server/tooltip.py
@@ -4,6 +4,7 @@
 # This file MUST BE an exact copy between
 # Desktop lizmap/tooltip.py
 # Server lizmap_server/tooltip.py
+# Except the CSS for LWC <= 3.7 and newer versions
 
 from __future__ import annotations
 
@@ -197,7 +198,7 @@ class Tooltip:
 
             if lvl > 1:
                 a += "\n" + SPACES * lvl + f'<fieldset class="{visibility}">'
-                a += "\n" + SPACES * lvl + f"<legend>{node.name()}</legend>"
+                a += "\n" + SPACES * lvl + f'<legend class="float-none">{node.name()}</legend>'
                 a += "\n" + SPACES * lvl + "<div>"
 
             # In case of root children
@@ -421,6 +422,7 @@ class Tooltip:
         )
 
     # CSS for LWC <= 3.7.
+    # For newer versions, the CSS in the lizmap-web-client is used.
     css = """<style>
     div.popup_lizmap_dd {
         margin: 2px;
