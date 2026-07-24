5d7e810f389c1f86465162a28df02c16107ca55c
elioooooott <eyvetot@3liz.com>
2026-07-24 12:00:42 +0200

    Fix single quotes escaping to avoid broken HTML output

--- lizmap_server/tooltip.py
+++ lizmap_server/tooltip.py
@@ -10,7 +10,6 @@ from __future__ import annotations
 
 import logging
 import re
-
 from textwrap import dedent
 
 from qgis.core import (
@@ -281,6 +280,8 @@ class Tooltip:
 
     @staticmethod
     def _generate_field_name(name: str, fname: str, expression: str) -> str:
+        # Escape single quotes to avoid broken HTML output
+        escaped_name = name.replace("'", "__escaped_quote__")
         return f'''
                     [%
                     concat(
@@ -292,14 +293,14 @@ class Tooltip:
                         END,
                         '">',
                         '    <label ',
-                '       id="dd_jforms_view_edition_{name}_label" ',
+                '       id="dd_jforms_view_edition_{escaped_name}_label" ',
                         '       class="control-label jforms-label" ',
-                '       for="dd_jforms_view_edition_{name}" >',
+                '       for="dd_jforms_view_edition_{escaped_name}" >',
                 '    {fname}',
                         '    </label>',
                         '    <div class="controls">',
                         '        <span ',
-                '            id="dd_jforms_view_edition_{name}" ',
+                '            id="dd_jforms_view_edition_{escaped_name}" ',
                         '            class="jforms-control-input" ',
                         '        >',
                             {expression},
@@ -385,7 +386,7 @@ class Tooltip:
                         '<a href="',
                         "{name}",
                         '" target="_blank">',
-                        base_file_name({name}),
+                        base_file_name("{name}"),
                         '</a>'
                     )'''
 
